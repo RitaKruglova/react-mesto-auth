27a0dd90f36bc54ac29f42cb9b6f0c01954ddb9c
Rita Kruglova <kruglova404@mail.ru>
2023-06-07 16:47:51 +0300

    feat: add header navigate links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,37 @@
 import logo from '../images/logo.svg';
+import { useLocation, Link, useNavigate } from 'react-router-dom';
 
-function Header() {
+function Header(props) {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate('/sign-in');
+  }
+
+  function getPath() {
+    if (location.pathname === '/sign-in') {
+      return (
+        <Link className="header__link" to='/sign-up'>Регистрация</Link>
+      )
+    } else if (location.pathname === '/sign-up') {
+      return (
+        <Link className="header__link" to='/sign-in'>Войти</Link>
+      )
+    } else {
+      return (
+        <ul className="header__info">
+          <li className="header__email">{props.email}</li>
+          <li className="header__link" onClick={handleClick}>Выйти</li>
+        </ul>
+      )
+    }
+  }
+
   return (
     <header className="header">
       <img src={logo} alt="Логотип Место" className="header__logo" />
+      {getPath()}
     </header>
   )
 }
