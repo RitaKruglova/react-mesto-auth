fb7a156eba8a0a8d9ede6e6b8ab45980c0f8bbca
Rita Kruglova <kruglova@404@mail.ru>
2023-06-13 18:59:27 +0300

    feat: add validation for login and fix login styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,33 +2,59 @@ import AuthenticationForm from "./AuthenticationForm";
 import { useState } from "react";
 import { authorise } from "../utils/auth";
 import { useNavigate } from "react-router-dom";
+import useValidate from "../hooks/useValidate";
+import { validateEmail, validatePassword } from "../utils/validation";
 
 function Login({ handleLogin, checkToken }) {
+  const EMAIL = 'email';
+  const PASSWORD = 'password';
   const navigate = useNavigate();
 
-  const [formValue, setFormValue] = useState({
-    email: '',
-    password: ''
-  })
-
-  function handleChange(event) {
-    const {name, value} = event.target;
-
-    setFormValue({
-      ...formValue,
-      [name]: value
-    })
+  function validate(values) {
+    const errors = {};
+    const emailError = validateEmail(values[EMAIL]);
+    const passwordError = validatePassword(values[PASSWORD]);
+    if (emailError) {
+      errors[EMAIL] = emailError;
+    }
+    if (passwordError) {
+      errors[PASSWORD] = passwordError;
+    }
+    return errors;
   }
+
+  const { setValues, values, errors, handleChange, isSubmitting } = useValidate({
+    [EMAIL]: '',
+    [PASSWORD]: ''
+  }, validate);
+
+  // const [formValue, setFormValue] = useState({
+  //   email: '',
+  //   password: ''
+  // })
+
+  // function handleChange(event) {
+  //   const {name, value} = event.target;
+
+  //   setFormValue({
+  //     ...formValue,
+  //     [name]: value
+  //   })
+  // }
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    authorise(formValue.email, formValue.password)
+    authorise(values[EMAIL], values[PASSWORD])
       .then(() => {
-        setFormValue({
-          email: '',
-          password: ''
-        });
+        setValues({
+          [EMAIL]: '',
+          [PASSWORD]: ''
+        })
+        // setFormValue({
+        //   email: '',
+        //   password: ''
+        // });
         handleLogin();
         checkToken();
         navigate('/', {replace: true});
@@ -41,7 +67,15 @@ function Login({ handleLogin, checkToken }) {
       title="Вход"
       onChange={handleChange}
       onSubmit={handleSubmit}
-      formValue={formValue}
+      formValue={values}
+      errors={errors}
+      isSubmitting={isSubmitting}
+      emailName={[EMAIL]}
+      passwordName={[PASSWORD]}
+      emailValue={values[EMAIL]}
+      passwordValue={values[PASSWORD]}
+      emailError={errors[EMAIL]}
+      passwordError={errors[PASSWORD]}
     />
   )
 }
