0d3541d973e144b47066f56f34f8c6b92993b6e8
Rita Kruglova <kruglova@404@mail.ru>
2023-06-14 20:13:33 +0300

    fix: add name of image in imagePopup

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -5,14 +5,14 @@ import { usePopupClose } from "../hooks/usePopupClose";
 function ImagePopup(props) {
   const { closeAllPopups } = useContext(AppContext);
 
-  usePopupClose(props.card, closeAllPopups);
+  usePopupClose(props.card?.link, closeAllPopups);
 
   return (
     <div className={`popup popup_type_picture${props.card ? ' popup_opened' : ''}`}>
       <div className="popup__container popup__container_place_popup-picture">
         <button type="button" className="popup__reset-button popup__reset-button_place_popup-picture" aria-label="Отменить" onClick={closeAllPopups}></button>
         <img className="popup__image" src={props.card?.link} alt={props.card?.name} />
-        <p className="popup__image-name"></p>
+        <p className="popup__image-name">{props.card?.name}</p>
       </div>
     </div>
   )
